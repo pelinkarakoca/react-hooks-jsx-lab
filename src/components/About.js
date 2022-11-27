66059f3ba6ac8b3b93625d411c529a013555a5eb
Pelin Karakoca <karakocapelin@gmail.com>
2022-11-28 00:42:31 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+  About
+    <h2>About Me</h2>
+    <p>Mucver is the silliest cat in the world. Her favorite toy is a drawstring and I am still, buying bunch of toys for this stupid face...</p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
